d5f0e7d27336c3464ecc737b841e7d0a2cb6710e
john king <johnking2010@me.com>
2018-09-21 16:47:57 +0100

    removed all but essential code for basic image display

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ var port = process.env.PORT || 3001;
 // make static files available: (ie - index.html)
 app.use(express.static('public'));
 
-// respond to 'GET' request:
+// respond to GET request - serve HTML:
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 // Lists the path to the files... (boring)
 app.get('/list', function(req, res) {
-  //console.log('Path to HTML', path.join(__dirname + '/index.html'));
   // to store correct paths for sending client-side:
   var collectArray = [];
   // - function to send TO the DOM!!!
@@ -32,15 +31,12 @@ app.get('/list', function(req, res) {
     .ext('.jpg')
     .find();
 
-  // ES6:
-  //files.then(files => { })
-  //--
   // ES5
   files.then(function(files) {
-    //console.log('\nListing ALL: ' + files.length +' files found: \n');
-    //console.log(files);
-    // ES6 style '=>' arrow function syntax
-    //files.forEach(file => console.log('Found files: ', file));
+    /* 
+    ES6 style '=>' arrow function syntax:
+    files.forEach(file => console.log('Found files: ', file)); 
+    */
     //---
     // ES5 style:
     files.forEach(function(file) {
@@ -52,23 +48,15 @@ app.get('/list', function(req, res) {
       var longPathLength = 0;
       var repeatedPath = __dirname + '/public';
       var repeatedPathLength = repeatedPath.length;
-      console.log('\n' + repeatedPathLength, 'chars long,' + ' repeated path: ' + repeatedPath);
+
       var shortPathLength = 0;
       longPathLength = file.length;
       cutPath = file.slice(0, repeatedPathLength); // 0, 52 - 1
       shortPath = file.slice(repeatedPathLength, longPathLength); // 52, end
-      // *** THIS ONE:
-      usefulRelPath = '.' + shortPath;
-      // ***
-      /*
-      console.log('short path: ', shortPath, 'cutPath: ', cutPath, 'usefulRelPath: ', usefulRelPath);
-      console.log('longPathLength:', longPathLength, 'repeatedPathLength: ', repeatedPathLength, 'diff : ', longPathLength - repeatedPathLength);
-      var tempImageOutput = "<img src=" +'"'+ file +'"' + " style=" +'"'+ "width:auto;height:auto;" +'"'+ ">";
-          console.log('tempImageOutput: ', tempImageOutput + '\n');
-          console.log('Found files: ', file + '\n');
-      */    
-
-      return collectArray.push(usefulRelPath); // REL file path
+      // useful:
+      usefulRelPath = '.' + shortPath;  
+      // relative file path:
+      return collectArray.push(usefulRelPath); 
     })
     // to DOM - just the top file path as text displayed...
     /*
@@ -76,75 +64,21 @@ app.get('/list', function(req, res) {
     res.send(collectArray);
     res.render('index', { data: JSON.stringify(collectArray) });
     */
-    console.log('----\n');
-
     // DO ONCE !
     sendPathsOn(collectArray);
   });
-
-  // png -
-  //console.log('Listing .png \n----\n');
-  var files = filehound.create()
-    .paths(path.join(__dirname + '/public/images/'))
-    .ext('png')
-    .find();
-    //.ext IS case-sensitive...
-  files.then(console.log);
-  //
-
-  /*
-  // ES6 fat arrow func, resolving promise:
-  .then(files => {
-    //
-    console.log('promise returning ? ' + files);
-    files.forEach(file => console.log('Found File', file));
-  });
-  */
-
-  // - JPG
-  //console.log('Listing .JPG \n----\n');
-  var files = filehound.create()
-    .paths(path.join(__dirname + '/public/images/'))
-    .ext('JPG')
-    .find();
-  // files.then(console.log);
-
-  // - jpg
-  //console.log('Listing .jpg \n----\n');
-  var files = filehound.create()
-    .paths(path.join(__dirname + '/public/images/'))
-    .ext('jpg')
-    .find();
-  // files.then(console.log);
-  //
-
-  // - gif
-  //console.log('Listing .gif \n----\n');
-  var files = filehound.create()
-    .paths(path.join(__dirname + '/public/images/'))
-    .ext('gif')
-    .find();
-  // files.then(console.log);
-  //
-
-  // - GIF
-  //console.log('Listing .GIF \n----\n');
-  var files = filehound.create()
-    .paths(path.join(__dirname + '/public/images/'))
-    .ext('GIF')
-    .find();
-  // files.then(console.log);
-  //
 });
 
 // FTP: (access via '/images' endpoint)
 app.use('/images', express.static('public/images'), serveIndex('public/images', {'icons': true, 'view': 'tiles'}))
 console.log('FTP Set Up - access via /images');
-  /* Serve URLs like /ftp/thing as public/ftp/thing
+/* 
+Serve URLs like /ftp/thing as public/ftp/thing
   The express.static serves the file contents
-  The serveIndex is this module serving the directory */
+  The serveIndex is this module serving the directory 
+*/
 
-//instantiate server: usual way
+// Instantiate server:
 app.listen(port, function() {
   console.log('Example app listening on port 3001');
 });
